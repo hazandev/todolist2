c37b665dc8ab96929f30fd640cf33e55fc790fab
rotemdev1 <60469936+hazandev@users.noreply.github.com>
2022-07-27 19:27:12 +0300

    add title and pre

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 export const App = () => {
+  const num = 12
+  console.log("number1");
   return (
     <div className="App">
       <h1>My First Project</h1>
+      <h4>i so exiting from this project</h4>
+      <pre>{num}</pre>
     </div>
   );
 }
